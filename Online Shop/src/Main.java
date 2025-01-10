6ef6d9534c6fc94e85c0f0bc83a393252f5b4d13
350179669 <350179669@gapps.yrdsb.ca>
2025-01-10 17:29:26 +0000

    Made product categories

--- Online Shop/src/Main.java
+++ Online Shop/src/Main.java
@@ -1,7 +1,25 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("h");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Amazon Shop");
+        System.out.println("ELECTRONICS " + "TOYS " + "BOOKS");
+        System.out.print("Enter category to view:");
+        String Category = sc.nextLine();
+
+        if (Category.toUpperCase().equals("ELECTRONICS")) {
+            System.out.println("you have selected ELECTRONICS");
+         } else if (Category.toUpperCase().equals("TOYS")) {
+            System.out.println("you have selected TOYS");
+         } else if (Category.toUpperCase().equals("BOOKS")) {
+            System.out.println("You have selected BOOKS");
+         } else {
+            System.out.println("Please enter a valid category!\nELECTRONICS | TOYS | BOOKS");
+         }
+         
+            sc.close();
         
+
     }
 }
