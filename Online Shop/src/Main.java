5f38442890cbae4e06513dec3e63be794932416e
350179669 <350179669@gapps.yrdsb.ca>
2025-01-13 17:53:43 +0000

    Finished Product Browser Co-authored-by: cheney460 <cheney460@users.noreply.github.com>

--- Online Shop/src/Main.java
+++ Online Shop/src/Main.java
@@ -17,9 +17,46 @@ public class Main {
          } else {
             System.out.println("Please enter a valid category!\nELECTRONICS | TOYS | BOOKS");
          }
+         String[] ProductNames = {"Iphone:", "Macbook:", "Ipad:", "Lego City:", "Nerf Gun:", "Barbie Doll:", "Diary of a Wimpy Kid:", "Captain Underpants:", "Dictionary:"};
+         Double[] ProductPrices = {1000.99, 1500.99, 800.99, 56.99, 68.99, 24.99, 13.99, 18.99, 26.99};
+         if (Category.toUpperCase().equals("ELECTRONICS")){
+            System.out.println("ELECTRONICS");
+         for (int i = 0; i < 3; i++) {
+            int ProductStock =(int) (Math.random() * 6);
+            System.out.println(ProductNames[i]);
+            System.out.println("$" + ProductPrices[i]);
+            System.out.println("# of items: " + ProductStock);
+           if (ProductStock == 0) {
+            System.out.println("Product is out of stock");
+           }
+         }
+         if (Category.toUpperCase().equals("TOYS")){
+            System.out.println("TOYS");
+         for (int i = 3; i < 6; i++) {
+            int ProductStock=(int) (Math.random() * 6);
+            System.out.println(ProductNames[i]);
+            System.out.println("# of items: " + ProductStock);
+         if (ProductStock == 0) {
+            System.out.println("Product is out of stock");
+            }
+            
+         }            
+         }
          
-            sc.close();
+         if (Category.toUpperCase().equals("BOOKS")){
+            System.out.println("BOOKS");
+            for (int i = 0; i < 3; i++){
+               int ProductStock = (int) (Math.random() * 6);
+               System.out.println(ProductNames[i]);
+               System.out.println("# of items: " + ProductStock);
+               if (ProductStock == 0) {
+                  System.out.println("Product is out of stock");
+               }
+            }
+         }
+
         
 
     }
-}
+   }
+   }
